ad4623692c478cf90ae2ad60ac34ce7697bd4d3d
Richmond Dominguez <richmonddominguez@gmail.com>
2022-06-15 14:08:13 -0700

    addedc connection function and user prompts/table selection

--- server.js
+++ server.js
@@ -9,3 +9,25 @@ const connection = mysql.createConnection({
   password: "rootroot",
   database: "scranton_db",
 });
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as ID" + connection.threadId);
+    console.log("Welcome to the Scranton Employee Database!")
+    initiateRun();
+});
+function initiateRun() {
+    inquirer.prompt([
+    {
+    type: "list",
+    message: "Please select a table to view.",
+    name: "choice",
+    choices: [
+              "View All Scranton Employees?", 
+              "View All Scranton Employees By Roles?",
+              "View All Scranton Employees By Deparments", 
+              "Update Scranton Employee",
+              "Add Employee to Scranton?",
+              "Add Role to Scranton?",
+              "Add Department to Scranton?"
+            ]
+    }
