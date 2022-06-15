91cceb6f2a3e35d7c96ef4f7efa906380354d520
Richmond Dominguez <richmonddominguez@gmail.com>
2022-06-14 21:49:27 -0700

    test run on mysql finally working, departments, employees, and roles populating tables

--- emp_schema.sql
+++ emp_schema.sql
@@ -4,21 +4,23 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR (30)
 );
 
 CREATE TABLE role (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR (30),
   salary DECIMAL,
-  department_id INT
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR (30),
   last_name VARCHAR (30),
+  manager_id INT,
   role_id INT,
-  manager_id INT
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
