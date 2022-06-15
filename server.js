bd0e76aa670d98274f0918be73e8a8ac11988f83
Richmond Dominguez <richmonddominguez@gmail.com>
2022-06-14 22:49:00 -0700

    started on server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const consoleTable = require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "scranton_db",
+});
