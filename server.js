fa419b2e91cc94309fd92813b802a02f840ab6c2
Richmond Dominguez <richmonddominguez@gmail.com>
2022-06-15 15:49:25 -0700

    added employee list function and employee role functions

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ function initiateRun() {
           break;
 
         case "Update Scranton Employee?":
-          updateScranEpm();
+          updateScranEmp();
           break;
 
         case "Add Employee to Scranton?":
@@ -64,3 +64,23 @@ function initiateRun() {
       }
     });
 }
+function viewScranEmp() {
+  connection.query(
+    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    function (err, res) {
+      if (err) throw err;
+      console.log("Scranton Employee List");
+      console.table(res);
+      initiateRun();
+    }
+  );
+}
+function viewScranRoles) {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+    function(err, res) {
+    if (err) throw err;
+    console.log("Scranton Employee Roles")
+    console.table(res)
+    initiateRun()
+    })
+  }
